43e08c715dc62c2d935ec7b93b605b8486b30df2
KimDaWoo <74904983+KimDaWoo@users.noreply.github.com>
2022-10-30 22:10:54 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Main from "./myTest/Main";
+import PassingCount from "./chapter_12/Sample1/PassingCount";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-    <Main />
+    <PassingCount />
 );
